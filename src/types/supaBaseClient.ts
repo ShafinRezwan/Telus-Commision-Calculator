e7f7e07d933c6d783e6f689f00e54e9a9378ecbc
ShafinRez <shafinrezwan123@gmail.com>
2025-08-07 19:34:27 -0600

    hiding api keys

--- src/types/supaBaseClient.ts
+++ src/types/supaBaseClient.ts
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+    // src/supabaseClient.js (or similar)
+    import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jmygurolntnzytfxzwut.supabase.co/';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpteWd1cm9sbnRuenl0Znh6d3V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MTM1OTMsImV4cCI6MjA2NjI4OTU5M30.3y8ngbGwgpqtzQXCxJEaj2AS5FVvQ_vc8rgI_6E5k7k";
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+    export const supabase = createClient(supabaseUrl, supabaseAnonKey);
